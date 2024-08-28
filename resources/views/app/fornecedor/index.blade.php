08463cf45731bbc7b09b14ff89651b92c0d6aa20
Lucas <lucas.fwielewski@gmail.com>
2024-08-28 19:15:20 -0300

    added isset

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,26 +1,21 @@
 <h3>Fornecedor</h3>
 
-@php
+<h3>Fornecedor</h3>
 
+@php
 @endphp
+
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[1]['nome'] }}
-    <br>
-    Status: {{ $fornecedores[1]['status'] }}
-    <br>
-    CNPJ: {{ $fornecedores[1]['cnpj'] ?? '' }}
-    <br>
-    Telefone: ({{ $fornecedores[1]['telefone'] ?? '' }}) {{ $fornecedores[1]['telefone'] ?? '' }}
-    @switch($fornecedores[1]['status'])
-        @case('11')
-            Sao Paulo - SP
-            @break
-        @case('32')
-            Juiz de Fora - MG
-            @break
-        @case('85')
-            Fortaleza - CE
-            @break 
-        @default
-            Estado não identificado       
+    @for ($i = 0; isset($fornecedores[$i]); $i++)
+        Fornecedor: {{ $fornecedores[$i]['nome'] }}
+        <br>
+        Status: {{ $fornecedores[$i]['status'] }}
+        <br>
+        CNPJ: {{ $fornecedores[$i]['cnpj'] ?? '' }}
+        <br>
+        Telefone: ({{ $fornecedores[$i]['telefone'] ?? '' }}) {{ $fornecedores[$i]['telefone'] ?? '' }}
+        <br>
+    @endfor
 @endisset
+
+<br>
