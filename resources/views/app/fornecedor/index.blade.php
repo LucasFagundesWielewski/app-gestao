dda6fabbca2fad9b74fcccd2f3c9ff8ffd92a6d7
Lucas <lucas.fwielewski@gmail.com>
2024-09-01 13:11:05 -0300

    feat: added while

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -6,7 +6,8 @@
 @endphp
 
 @isset($fornecedores)
-    @for ($i = 0; isset($fornecedores[$i]); $i++)
+    @php $i = 0 @endphp
+    @while(isset($fornecedores[$i]))
         Fornecedor: {{ $fornecedores[$i]['nome'] }}
         <br>
         Status: {{ $fornecedores[$i]['status'] }}
@@ -15,7 +16,8 @@
         <br>
         Telefone: ({{ $fornecedores[$i]['telefone'] ?? '' }}) {{ $fornecedores[$i]['telefone'] ?? '' }}
         <br>
-    @endfor
+        @php $i++ @endphp
+    @endwhile
 @endisset
 
 <br>
